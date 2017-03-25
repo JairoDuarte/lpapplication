76cb8141574c8db8005e8db58fd448a346794dc8
Dermoumi S <sdermoumi@gmail.com>
2017-03-25 08:31:12 +0000

    Amélioré le design mobile un peu

--- lpapplication/settings.py
+++ lpapplication/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config.get('secrets', 'SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config.get('debug', 'DEBUG')
 
-ALLOWED_HOSTS = ['192.168.137.1']
+ALLOWED_HOSTS = ['192.168.137.1', 'localhost']
 
 
 # Application definition
